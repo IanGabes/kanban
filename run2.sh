ccbd972f403e1321bd5441b18f3eb86beac2e5fa
iangabes <iangabriel0@gmail.com>
2017-01-18 14:54:39 -0500

    another typo fix? help me im stupid

---
+++ run2.sh
@@ -0,0 +1,2 @@
+sudo ./kanban server --redis-addr localhost:6379 --gitlab-url http://localhost --gitlab-client 57a0d2e1c9dc892364c4a92ea24fd298ce3b33067ae6401517097d26f783715c --gitlab-secret 650a5478ee542877c05cc11c50434e59d2dad7c17ef3cf1cebeff506c4a0554b --server-listen 127.0.1.1:9000 --server-hostname http://ubuntu.local:9000
+
